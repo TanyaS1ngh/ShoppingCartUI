818763386fdade9268c228c6ded5fe91fd3cc90a
tansingh7 <tanya.singh@publicissapient.com>
2022-01-13 23:19:08 +0530

    Navbar initialised

---
+++ shopping-cart/src/components/Navbar/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles.css'
+
+const Navbar = () => {
+    return (
+        <div className="grid navbar">
+            <div className='col-2 col-offset-5 flex justify-content-center align-items-center'>
+                Navbar temp text
+            </div>
+            
+        </div>
+    )
+}
+
+export default Navbar
